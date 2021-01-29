aeb8f4a097a4aab4ee4f30026787c4d05609ded1
tortoolkit <78198006+tortoolkit@users.noreply.github.com>
2021-01-29 22:52:14 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -7,8 +7,8 @@ except:
         IS_VPS = False
         API_HASH = "d0def2fadec58c31403bf01a71454019"
         API_ID = 2876934
-        BOT_TOKEN = "1632934464:AAHJfE73wJu8xbrlipSwHHnbCiYfAf2DBEE"
-        BASE_URL_OF_BOT = "nobitest001.com"
+        BOT_TOKEN = "1662323956:AAEij3cLkUsxMiLrnnnSlLxK_Ngz4rWX9UE"
+        BASE_URL_OF_BOT = "https://tortoolkit001.herokuapp.com/"
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
         ALD_USR = [1458912827,1467018377,1546675358,1444150889,1482853408,1333689035,992574970,-100374414317,-1001479070385,-100320640070]
         
@@ -31,7 +31,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "postgres://vctlpcqtnwgxqf:be33f1d37551bafa72e319416f572a1f5ed31446edfe5d34f10ce8a0d339f7c4@ec2-35-172-73-125.compute-1.amazonaws.com:5432/d2jlirm6b4ke5l"
+        DB_URI = "postgres://faoagbilehzlib:da9ed9d3f7acd6bdc1f0104644136c4ea89ae82780f2f9cc895d64a5bf8bc159@ec2-54-237-135-248.compute-1.amazonaws.com:5432/d264fq65lhh02c"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
