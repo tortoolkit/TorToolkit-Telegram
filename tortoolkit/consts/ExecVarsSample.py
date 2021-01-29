b434ee6bb0a89b510e3dfab2b9cbd102ab7d8f63
kwotiseerde <78017482+kwotiseerde@users.noreply.github.com>
2021-01-29 11:06:42 +0530

    Update ExecVarsSample.py

--- tortoolkit/consts/ExecVarsSample.py
+++ tortoolkit/consts/ExecVarsSample.py
@@ -5,12 +5,12 @@ except:
         # TODO optimize for vps use fully - currently only heroku is focused
         # Set true if its VPS [currently not fully working]
         IS_VPS = False
-        API_HASH = ""
-        API_ID = 0
-        BOT_TOKEN = ""
-        BASE_URL_OF_BOT = ""
+        API_HASH = "d0def2fadec58c31403bf01a71454019"
+        API_ID = 2876934
+        BOT_TOKEN = "1632934464:AAHJfE73wJu8xbrlipSwHHnbCiYfAf2DBEE"
+        BASE_URL_OF_BOT = "nobitest001.com"
         # ALLOWED USERS [ids of user or supergroup] seperate by commas
-        ALD_USR = []
+        ALD_USR = [1458912827,1467018377,1546675358,1444150889,1482853408,1333689035,992574970,-100374414317,-1001479070385,-100320640070]
         
         # Google Drive Index Link should include the base dir also See readme for more info
         GD_INDEX_URL = False
@@ -31,7 +31,7 @@ except:
         REMAINING_STR = "▱"
 
         # DB URI for access
-        DB_URI = "dbname=tortk user=postgres password=your-pass host=127.0.0.1 port=5432"
+        DB_URI = "postgres://vctlpcqtnwgxqf:be33f1d37551bafa72e319416f572a1f5ed31446edfe5d34f10ce8a0d339f7c4@ec2-35-172-73-125.compute-1.amazonaws.com:5432/d2jlirm6b4ke5l"
         
         # UNCOMMENT THE BELOW LINE WHEN USING CONTAINER AND COMMENT THE UPPER LINE
         #DB_URI = "dbname=tortk user=postgres password=your-pass host=db port=5432"
